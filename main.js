a95984e217f6a9cfbded38ff146cdc6ac3659f20
Cues STEM <cuesstemschool@gmail.com>
2026-09-14 18:47:12 +0200

    WIP
    am currently adding an "article" class and i dont have to finish it

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ function toggleMobileMenu() {
   const menu = document.getElementById("mobile-menu");
   menu.classList.toggle("hidden");
 }
+class Article {
+  constructor(id, page, title, description, text) {
+    this.id = id;
+    this.page = page;
+    this.title = title;
+    this.description = description;
+    this.text = text;
+  }
+}
